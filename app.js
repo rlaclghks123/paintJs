9b772fc33ccaa7e39465e30789a9f8edc9cd3d01
Chi Hwan Kim <rlaclghks123@naver.com>
2022-03-31 23:27:18 +0900

    Js -canvas작업 2
    canvas안에 선을 나타내도록 한다.
    ctx.beginPath(), moveTo(x,y)사용,
    ctx.lineTo(), ctx.stroke()사용

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+
+canvas.width = 600;
+canvas.height = 600;
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
 
 let painting = false;
+
+function startPainting() {
+    painting = true;
+}
 
 function stopPainting() {
     painting = false;
@@ -9,19 +21,21 @@ function stopPainting() {
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
-function onMouseDown(event) {
-    painting = true;
-}
 
-function onMouseUp(event) {
-    stopPainting();
-}
+
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
